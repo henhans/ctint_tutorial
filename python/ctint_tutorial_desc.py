5d572f4208f3c292401767dd3473faf566d73ac4
Olivier Parcollet <olivier.parcollet@cea.fr>
2015-04-07 22:55:11 +0200

    regenerate the desc ...
    to have the right call to c++2py.
    no diff except spaces and main comment at top.

--- python/ctint_tutorial_desc.py
+++ python/ctint_tutorial_desc.py
@@ -1,6 +1,5 @@
-
-# Generated automatically using the command : 
-# wrapper_desc_generator.py ../c++/ctint.hpp -p -m pytriqs.applications.impurity_solvers.ctint_tutorial -o ctint_tutorial
+# Generated automatically using the command :
+# c++2py.py ../c++/ctint.hpp -p -m pytriqs.applications.impurity_solvers.ctint_tutorial -o ctint_tutorial
 from wrap_generator import *
 
 # The module
@@ -23,25 +22,24 @@ c = class_(
         c_type = "ctint_solver",   # name of the C++ class
 )
 
-c.add_constructor("""(double beta_, int n_iw = 1024, int n_tau = 100001)""", 
+c.add_constructor("""(double beta_, int n_iw = 1024, int n_tau = 100001)""",
                   doc = """ """)
 
-c.add_method("""void solve (double U, double delta, int n_cycles, int length_cycle = 50, int n_warmup_cycles = 5000, std::string random_name = "", int max_time = -1)""", 
+c.add_method("""void solve (double U, double delta, int n_cycles, int length_cycle = 50, int n_warmup_cycles = 5000, std::string random_name = "", int max_time = -1)""",
              doc = """ """)
 
-c.add_property(name = "G0_iw", 
+c.add_property(name = "G0_iw",
                getter = cfunction("block_gf_view<imfreq> G0_iw ()"),
                doc = """ """)
 
-c.add_property(name = "G0_tau", 
+c.add_property(name = "G0_tau",
                getter = cfunction("block_gf_view<imtime> G0_tau ()"),
                doc = """ """)
 
-c.add_property(name = "G_iw", 
+c.add_property(name = "G_iw",
                getter = cfunction("block_gf_view<imfreq> G_iw ()"),
                doc = """ """)
 
 module.add_class(c)
 
 module.generate_code()
-
